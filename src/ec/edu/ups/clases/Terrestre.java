a437429aff95222fa1d88514a4d2c70217612488
ivan <ivan@192.168.5.143>
2019-04-12 19:48:42 -0500

    Todas las clases deben tener por lo menos cuatros atributos (aparte de los de herencia), tres métodos (aparte de los getters y setters) y método toString().

--- src/ec/edu/ups/clases/Terrestre.java
+++ src/ec/edu/ups/clases/Terrestre.java
@@ -9,6 +9,67 @@ package ec.edu.ups.clases;
  *
  * @author ivan
  */
-public class Terrestre {
-    
+public abstract class Terrestre extends Animal {
+
+    private int numPatas;
+    private String colorPelaje;
+    private String tamañaOrejas;
+    private String tamañoNariz;
+
+    public Terrestre() {
+    }
+
+    public Terrestre(int numPatas, String colorPelaje, String tamañaOrejas, String tamañoNariz) {
+        this.numPatas = numPatas;
+        this.colorPelaje = colorPelaje;
+        this.tamañaOrejas = tamañaOrejas;
+        this.tamañoNariz = tamañoNariz;
+    }
+
+    public Terrestre(int numPatas, String colorPelaje, String tamañaOrejas, String tamañoNariz, String tipoAnimal, int peso, int tamaño, String habitad) {
+        super(tipoAnimal, peso, tamaño, habitad);
+        this.numPatas = numPatas;
+        this.colorPelaje = colorPelaje;
+        this.tamañaOrejas = tamañaOrejas;
+        this.tamañoNariz = tamañoNariz;
+    }
+
+    public int getNumPatas() {
+        return numPatas;
+    }
+
+    public void setNumPatas(int numPatas) {
+        this.numPatas = numPatas;
+    }
+
+    public String getColorPelaje() {
+        return colorPelaje;
+    }
+
+    public void setColorPelaje(String colorPelaje) {
+        this.colorPelaje = colorPelaje;
+    }
+
+    public String getTamañaOrejas() {
+        return tamañaOrejas;
+    }
+
+    public void setTamañaOrejas(String tamañaOrejas) {
+        this.tamañaOrejas = tamañaOrejas;
+    }
+
+    public String getTamañoNariz() {
+        return tamañoNariz;
+    }
+
+    public void setTamañoNariz(String tamañoNariz) {
+        this.tamañoNariz = tamañoNariz;
+    }
+
+    public abstract String caminar();
+
+    @Override
+    public String toString() {
+        return super.toString() + "Terrestre{" + "numPatas=" + numPatas + ", colorPelaje=" + colorPelaje + ", tama\u00f1aOrejas=" + tamañaOrejas + ", tama\u00f1oNariz=" + tamañoNariz + '}';
+    }
 }
